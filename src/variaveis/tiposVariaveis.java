1fb3d7b54e33d1db3cb41ac78481a0a0c5ab558c
pierresouza <pierre.s3@hotmail.com>
2025-02-16 16:44:56 -0300

    feat: types of variables

---
+++ src/variaveis/tiposVariaveis.java
@@ -0,0 +1,10 @@
+package variaveis;
+
+public class tiposVariaveis {
+    double salarioMinimo = 2500;
+
+    short numeroCruto = 1;
+    int numeroNormal = numeroCruto;
+    short numeroCurto2 = (short)  numeroNormal;
+
+}
